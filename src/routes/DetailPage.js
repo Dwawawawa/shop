4f9b2ab39f864b853b467b33667d4bec2c07dc1e
JANG HYO JE <113086153+Dwawawawa@users.noreply.github.com>
2024-04-12 19:09:16 +0900

    무자커피에서는 콘센트 못써

--- src/routes/DetailPage.js
+++ src/routes/DetailPage.js
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
 import {useParams} from "react-router-dom"
+import styled from "styled-components";
+
+let Box = styled.div`
+  background : yellow;
+  padding : 20px;
+  color : grey;
+`;
 
 function DetailPage(props){
   
+  useEffect(()=>{
+    console.log('안녕')
+  })
+
+  setTimeout(() => {
+    
+  }, 2000);
+
+  let [count, setCount] = useState(0)
+
   let {id} = useParams();  
   //id로 숫자(우리가 원하는 항목의 id와 동일하자나?)
   let product = props.shoes.find((shoe)=> shoe.id === parseInt(id));
@@ -12,7 +30,10 @@ function DetailPage(props){
 
   return(
       <div className="container">
-        
+{/* 디테일 페이지 방문 후 2초지나면 <div/> 숨기기 */}
+        <div className = "alret alret-warning">2초이내 구매시 할인</div>
+        {count}
+        <button onClick={()=>{setCount(count + 1)}}>버튼</button>        
         <div className="row">
           <div className="col-md-6">
             <img src={product.src} width="100%" />
