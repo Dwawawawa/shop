e6ca8f25d8d0bb89d7470a8b5f4bddf9018de9eb
JANG HYO JE <113086153+Dwawawawa@users.noreply.github.com>
2024-05-20 14:06:35 +0900

    feat: 일괄 적용

--- src/App.js
+++ src/App.js
@@ -1,57 +1,119 @@
-import { useState} from 'react';
+/*eslint-disable*/
+import React, { useState, useContext, lazy, Suspense } from 'react';
+import { Navbar, Container, Nav, NavDropdown, Button } from 'react-bootstrap';
 import './App.css';
-import { Navbar, Container, Nav} from 'react-bootstrap';
-import data from './data.js';
-import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom'
-import DetailPage from './routes/DetailPage.js'
-import MainPage from './routes/MainPage.js';
+import Data from './data.js';
+import { Link, Route, Routes, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Cart from './Cart.js';
 
+const Detail = lazy(() => import('./Detail.js'));
+
+export const 재고context = React.createContext();
 
 function App() {
-  let [shoes] = useState(data)
-  let navigate = useNavigate();
+  let [shoes, shoes변경] = useState(Data);
+  let [재고, 재고변경] = useState([10, 11, 12]);
 
   return (
     <div className="App">
-      <Navbar bg="primary" data-bs-theme="dark" className=''>
+      <Navbar bg="light" expand="lg">
         <Container>
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/">홈</Nav.Link>
-            <Nav.Link href="/detail">상세페이지</Nav.Link>
-            <Nav.Link onClick={()=> navigate('/event')}>이벤트</Nav.Link>
-          </Nav>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/detail">Detail</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
 
-
-
       <Routes>
-        <Route path= "/" element ={<MainPage shoes={shoes} />} />
-        <Route path= "/detail" element ={<DetailPage shoes={shoes}/>} />
-        <Route path='/detail/:id' element ={<DetailPage shoes={shoes}/>} />
-        <Route path= "/event" element ={<Event></Event>}>
-          <Route path='one' element={<div>첫 주문시 양배추즙 서비스</div>}></Route>
-          <Route path='two' element={<div>생일기념 쿠폰받기</div>}></Route>
-        </Route>
-        
-        <Route path= "*" element ={<div>404</div>} />
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="jumbotron">
+                <h1>20% Season Off!</h1>
+                <p>
+                  This is a simple hero unit, a simple jumbotron-style component for calling
+                  extra attention to featured content or information.
+                </p>
+                <p>
+                  <Button variant="primary">Learn more</Button>
+                </p>
+              </div>
+              <div className="container">
+                <재고context.Provider value={재고}>
+                  <div className="row">
+                    {shoes.map((a, i) => {
+                      return <Card shoes={shoes[i]} i={i} key={i} />;
+                    })}
+                  </div>
+                </재고context.Provider>
+                <button
+                  className="btn btn-primary"
+                  onClick={() => {
+                    axios
+                      .get('https://codingapple1.github.io/shop/data2.json')
+                      .then((result) => {
+                        shoes변경([...shoes, ...result.data]);
+                      })
+                      .catch(() => {
+                        console.log('실패');
+                      });
+                  }}
+                >
+                  더보기
+                </button>
+              </div>
+            </>
+          }
+        />
+
+        <Route
+          path="/detail/:id"
+          element={
+            <재고context.Provider value={재고}>
+              <Suspense fallback={<div>로딩중이에요</div>}>
+                <Detail shoes={shoes} 재고={재고} 재고변경={재고변경} />
+              </Suspense>
+            </재고context.Provider>
+          }
+        />
+
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </div>
   );
 }
 
-function Event(){
+function Card(props) {
+  let 재고 = useContext(재고context);
+  let navigate = useNavigate();
+
   return (
-    <div>
-      <h4>오늘의 이벤트</h4>
-      <Outlet></Outlet>
-      <Link to="/event/one">1</Link>
-      <br></br>
-      <Link to="/two">2</Link>
+    <div className="col-md-4" onClick={() => navigate('/detail/' + props.shoes.id)}>
+      <img src={'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'} width="100%" />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.content} & {props.shoes.price}</p>
+      <Test />
     </div>
-  )
+  );
 }
 
+function Test() {
+  let 재고 = useContext(재고context);
+  return <p>{재고[0]}</p>;
+}
 
 export default App;
---
+++ src/Detail.js
@@ -0,0 +1,135 @@
+/*eslint-disable*/
+import React, { useState, useEffect, useContext } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import './Detail.scss';
+import { 재고context } from './App.js';
+import { CSSTransition } from 'react-transition-group';
+import { Nav } from 'react-bootstrap';
+import { connect } from 'react-redux';
+
+const 박스 = styled.div`
+  padding: 20px;
+`;
+
+const 제목 = styled.h4`
+  font-size: 25px;
+  color: ${(props) => props.색상};
+`;
+
+function Detail(props) {
+  let [alert, alert변경] = useState(true);
+  let [누른탭, 누른탭변경] = useState(0);
+  let [스위치, 스위치변경] = useState(false);
+
+  let 재고 = useContext(재고context);
+
+  useEffect(() => {
+    let 타이머 = setTimeout(() => {
+      alert변경(false);
+    }, 2000);
+    return () => {
+      clearTimeout(타이머);
+    };
+  }, []);
+
+  let { id } = useParams();
+  let navigate = useNavigate();
+  let 찾은상품 = props.shoes.find((x) => x.id == id);
+
+  useEffect(() => {
+    let arr = localStorage.getItem('watched');
+    if (arr == null) {
+      arr = [];
+    } else {
+      arr = JSON.parse(arr);
+    }
+
+    arr.push(id);
+    arr = new Set(arr);
+    arr = [...arr];
+
+    localStorage.setItem('watched', JSON.stringify(arr));
+  }, []);
+
+  return (
+    <div className="container">
+      <박스>
+        <제목 className="red">Detail</제목>
+      </박스>
+
+      {alert === true ? (
+        <div className="my-alert2">
+          <p>재고가 얼마 남지 않았습니다</p>
+        </div>
+      ) : null}
+      <div className="row">
+        <div className="col-md-6">
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+        </div>
+        <div className="col-md-6 mt-2">
+          <h4 className="pt-5">{찾은상품.title}</h4>
+          <p>{찾은상품.content}</p>
+          <p>{찾은상품.price}원</p>
+
+          <Info 재고={props.재고} />
+
+          <button
+            className="btn btn-danger"
+            onClick={() => {
+              props.재고변경([9, 11, 12]);
+              props.dispatch({ type: '항목추가', 데이터: { id: 찾은상품.id, name: 찾은상품.title, quan: 1 } });
+              navigate('/cart');
+            }}
+          >
+            주문하기
+          </button>
+          &nbsp;
+          <button className="btn btn-danger" onClick={() => navigate('/')}>
+            뒤로가기
+          </button>
+        </div>
+      </div>
+
+      <Nav className="mt-5" variant="tabs" defaultActiveKey="link-0">
+        <Nav.Item>
+          <Nav.Link eventKey="link-0" onClick={() => { 스위치변경(false); 누른탭변경(0); }}>상품설명</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1" onClick={() => { 스위치변경(false); 누른탭변경(1); }}>배송정보</Nav.Link>
+        </Nav.Item>
+      </Nav>
+
+      <CSSTransition in={스위치} classNames="wow" timeout={500}>
+        <TabContent 누른탭={누른탭} 스위치변경={스위치변경} />
+      </CSSTransition>
+    </div>
+  );
+}
+
+function TabContent(props) {
+  useEffect(() => {
+    props.스위치변경(true);
+  });
+
+  if (props.누른탭 === 0) {
+    return <div>0번째 내용입니다</div>;
+  } else if (props.누른탭 === 1) {
+    return <div>1번째 내용입니다</div>;
+  } else if (props.누른탭 === 2) {
+    return <div>2번째 내용입니다</div>;
+  }
+}
+
+function Info(props) {
+  return <p>재고: {props.재고[0]}</p>;
+}
+
+function state를props화(state) {
+  return {
+    state: state.reducer,
+    alert열렸니: state.reducer2,
+  };
+}
+
+export default connect(state를props화)(Detail);
